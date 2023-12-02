228b4fad7fdee6f4e693b43b85f3f6eebfa27d48
Anna Wojtowicz <annwoj26185@stud.noroff.no>
2023-12-02 13:40:26 +0100

    install bootstrap

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // import './style.css'
 import "./style.scss";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
