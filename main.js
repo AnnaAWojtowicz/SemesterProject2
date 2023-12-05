4040236d0932b3e9860e67fc6fb27eb8d97bddac
Anna Wojtowicz <annwoj26185@stud.noroff.no>
2023-12-05 19:50:01 +0100

    install bootstrap-icons

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // import './style.css'
-import "./style.scss";
+
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.js";
+import "./style.scss";
 // import javascriptLogo from "./javascript.svg";
 // import viteLogo from "/vite.svg";
 // import { setupCounter } from "./counter.js";
